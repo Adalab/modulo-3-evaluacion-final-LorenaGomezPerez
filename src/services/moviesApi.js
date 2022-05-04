2e36b2cdb26adb7991fbd9e9e82eeb0299e984dd
LorenaGomezPerez <lorenagomezperez@gmail.com>
2022-05-04 19:41:03 +0200

    moviesApi create

---
+++ src/services/moviesApi.js
@@ -0,0 +1,23 @@
+
+// Función donde hacemos una petición al servidor, y que me devuelve una serie de datos
+
+const getDataApi = () => {
+    return fetch('https://owen-wilson-wow-api.herokuapp.com/wows/random?results=50')
+    
+    .then((response)=>response.json())
+    .then((data)=>{
+        const dataReduced = data.map((task)=>{
+        return {
+            poster: task.poster,
+            movie: task.movie,
+            task: task.full_line,
+            year: task.year,
+            director: task.director,
+            audio: task.audio,
+            };
+        });
+        return dataReduced;
+    });
+};
+
+export default getDataApi;
